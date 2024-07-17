1f990e0a8bc8ba4312dae4a5ca2eb9e6cd7050d3
Olha Bezklepchenko <krolichek.11@gmail.com>
2024-07-18 00:13:06 +0200

    add timer

---
+++ src/js/1-timer.js
@@ -0,0 +1,91 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const dateInput = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('button[data-start]');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
+
+startButton.disabled = true;
+let userSelectedDate = null;
+let countdownInterval = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      startButton.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr(dateInput, options);
+
+startButton.addEventListener('click', onClick);
+
+// Колбек для обробки події натискання на кнопку "Start"
+function onClick() {
+  if (userSelectedDate) {
+    startCountdown(userSelectedDate);
+    startButton.disabled = true;
+    dateInput.disabled = true;
+  }
+}
+
+function startCountdown(endDate) {
+  countdownInterval = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = endDate - now;
+
+    if (distance < 0) {
+      clearInterval(countdownInterval);
+      dateInput.disabled = false;
+      startButton.disabled = true;
+      return;
+    }
+
+    const convertedTime = convertMs(distance);
+    updateTimerDisplay(convertedTime);
+  }, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor((ms % hour) / minute);
+  const seconds = Math.floor((ms % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
+  daysValue.textContent = addLeadingZero(days);
+  hoursValue.textContent = addLeadingZero(hours);
+  minutesValue.textContent = addLeadingZero(minutes);
+  secondsValue.textContent = addLeadingZero(seconds);
+}
